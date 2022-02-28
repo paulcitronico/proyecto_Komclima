b32e18b92cb1ce264b7493bf01e2ffb829baeec0
Eliaser Concha <eliaserdev99@gmail.com>
2022-02-28 16:52:37 -0300

    Mantener la sesión de un usuario autenticado

--- routes/root.py
+++ routes/root.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect
+from flask_login import login_required, current_user
 from models.Forms.LoginForm import LoginForm
 
 root = Blueprint("root",__name__)
 
 @root.route("/")
 def index():
+    if current_user.is_authenticated:
+        return redirect("profile")
+
     return render_template("index.html", css="index.css", form=LoginForm())
 
 @root.route("/profile")
