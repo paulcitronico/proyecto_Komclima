e2dbb61c8441071e06fca10ee791f30147bf8ddb
Eliaser Concha <eliaserdev99@gmail.com>
2022-05-24 17:47:32 -0400

    Vista: Account

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 dashboard = Blueprint("dashboard",__name__)
 
@@ -7,3 +8,9 @@ dashboard = Blueprint("dashboard",__name__)
 # Visión general
 
 # Panel de servicio
+
+# Ajustes de la cuenta del usuario
+@dashboard.route("/account")
+@login_required
+def my_account():
+    return render_template("account.html",css="css/account.css")
